5ed199b9316f545ab4421f60739a75368247bd34
asdf <ajoshi@micds.org>
2024-08-23 11:00:35 -0500

    added code

---
+++ EX01/EX01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int agrc, char const *argv[]){
+    //argc is the number of strings given to this program
+    //argv is the array of strings
+    cout<<"Hello, World!"<<endl;
+    //This is a comment
+    /*
+    This is a multiiline comment
+    */
+   int x = 3;
+   cout<<"The number x is "<<x<<endl;
+   int y;
+   cout<<"Please enter a number of y: ";
+   cin>>y;
+   cout<<"The number y is "<<y<<endl;
+   /*Data types in C++ are:
+        int, float, double, bool, and char
+   */
+  int a;
+  cout<<"The value of a is "<<a<<endl;
+}
